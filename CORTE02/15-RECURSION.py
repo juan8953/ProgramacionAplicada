59481df230125521f0fe9bac13bf96bb7ff96ee1
juan8953 <110635251+juan8953@users.noreply.github.com>
2022-09-15 12:57:51 -0500

    Update 15-RECURSION.py

--- CORTE02/15-RECURSION.py
+++ CORTE02/15-RECURSION.py
@@ -19,10 +19,33 @@ def f_contar(s_num): # se inicia la función, esta debe tener un parametro que s
 
 print(f_contar(1))
 
-# TODO: modificar esta función para que imprima todos los valores de manera ordenada
+#TODO: modificar esta función para que imprima todos los valores de manera ordenada
+def f_contar(s_num): # se inicia la función, esta debe tener un parametro que sirva como memoria del sistema
+    if s_num < 10: # si el número actual es menor que 10, aumentar en uno y volver a correr la función
+        print(s_num, end = " ") #imprimimos el numero que va en secuencia nates de la suma , ejemplo= imprime el uno , cumple con la condiciomn y recorre la funcion , como se sumo a un 2 , imprime el 2 y hace el mismo recorrido y asi sucesivamente hasta cumplir la condicion
+        s_num+=1
+        return f_contar(s_num) # al correr nuevamente la función, ingresa con s_num + 1 como parametro por lo que cada vez incrementa
+    else:
+        return s_num # finalmetne cuando se cumple la condición se retorna el valor final
+print(f_contar(1))
 # TODO: modificar la función para que reciba dos parametros, el número inicial y el objetivo
+def f_contar(s_num,s_objeto): # se inicia la función, esta debe tener 2  parametros que sirvan como memoria del sistema
+    if s_num < s_objeto: # si el numero actual dado por el parametro es menor que el numero dado por el otro parametro , entonces realiza el recorrido en la funcion
+        print(s_num, end = " ") #imprimimos el numero que va en secuencia nates de la suma , ejemplo= imprime el uno , cumple con la condiciomn y recorre la funcion , como se sumo a un 2 , imprime el 2 y hace el mismo recorrido y asi sucesivamente hasta cumplir la condicion
+        s_num+=1
+        return f_contar(s_num,s_objeto) # al correr nuevamente la función, ingresa con s_num + 1 como parametro por lo que cada vez incrementa
+    else:
+        return s_num # finalmetne cuando se cumple la condición se retorna el valor final
+print(f_contar(1,12))
 # TODO: modificar la función para que reciba 3 parametros el número inicial, el objetivo y los pasos entre números
-
+def f_contar(s_num,s_objeto,s_pasos): # se inicia la función, esta debe tener 2  parametros que sirvan como memoria del sistema
+    if s_num < s_objeto: # si el numero actual dado por el parametro es menor que el numero dado por el otro parametro , entonces realiza el recorrido en la funcion
+        print(s_num, end = " ")#imprimimos el numero que va en secuencia nates de la suma , ejemplo= imprime el uno , cumple con la condiciomn y recorre la funcion , como se sumo a un 2 , imprime el 2 y hace el mismo recorrido y asi sucesivamente hasta cumplir la condicion
+        s_num+=s_pasos  #se utiliza el parametro incial para realziar el recorrido y se le pide que sea la suma igual al parametro de los pasos
+        return f_contar(s_num,s_objeto,s_pasos) # al correr nuevamente la función, ingresa con s_num + 1 como parametro por lo que cada vez incrementa
+    else:
+        return s_num # finalmetne cuando se cumple la condición se retorna el valor final
+print(f_contar(1,12,2))
 ## Torre de Hanoi
 '''
 existen problemas que son por naturaleza recursivos, un ejemplo de estos es la torre de hanoi 
